673e943b70b8fdcc6c231ec8735b9a703558e4a9
Bartosz Malec <bartosz.malec@onet.pl>
2026-02-09 20:19:27 +0100

    automatically calculate current year in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import {TranslateModule, TranslateService} from "@ngx-translate/core";
 import {Footer} from "./footer.model";
-import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
     TranslateModule,
-    NgIf
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
+  currentYear = new Date().getFullYear();
   model?: Footer;
 
   constructor(private translate: TranslateService) {}
